c86334cd5355bad042481af2b2b27d49c80ea6a5
microstorage <microstorage@users.noreply.github.com>
2017-01-15 18:57:19 +0100

    My commit

--- it/0/po/potenziamento-muscolare.js
+++ it/0/po/potenziamento-muscolare.js
@@ -1,4 +1,3 @@
 Gr = [
 "Panca Ginnica e Panca per Addominali tutto in uno!|Victoria Kit Panca Bench Trainer + Manubri + Pesi e trasporto gratis!|2017/01/panca-ginnica-e-panca-per-addominali-tutto-in-uno|data.oliviero.it/imgprodotto/kit-panca-bench-trainer-manubri-pesi_141090",
-"Una delle migliori Panche a Inversione sul mercato|Panca inversione Inversion Comfort Pro, per tenersi in forma da casa|2017/01/una-delle-migliori-panche-a-inversione-sul-mercato|data.oliviero.it/imgprodotto/panca-inversione-inversion-comfort-pro_96358",
 ];
