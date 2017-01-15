01e9bf2facb572acff34d5c030329106779911d9
microstorage <microstorage@users.noreply.github.com>
2017-01-15 18:07:58 +0100

    My commit

--- it/0/index.js
+++ it/0/index.js
@@ -1,4 +1,5 @@
 Gr = [
+"Apriscatole con Levacapsule incorporato|Un Apriscatole con Levacapsule, per una vita facile in cucina!|2017/01/apriscatole-con-levacapsule-incorporato|data.oliviero.it/imgprodotto/apriscatole-con-levacapsule-420258_155155",
 "Casco Sci Comanche 2 Pure: comfort e resistenza|Un casco da sci confortevole e professionale, adatto a tutti gli sciatori|2017/01/casco-sci-comanche-2-pure-comfort-e-resistenza|data.oliviero.it/imgprodotto/casco-sci-comanche-2-pure-s5661572105_92186",
 "Una delle migliori Panche a Inversione sul mercato|Panca inversione Inversion Comfort Pro, per tenersi in forma da casa|2017/01/una-delle-migliori-panche-a-inversione-sul-mercato|data.oliviero.it/imgprodotto/panca-inversione-inversion-comfort-pro_96358",
 "Stiratura a secco e a vapore ai massimi livelli|Sistema Stirante 2100 watt - 50.939 - per una stiratura perfetta e veloce|2017/01/stiratura-a-secco-e-a-vapore-ai-massimi-livelli|data.oliviero.it/imgprodotto/sistema-stirante-2100-watt-50-939_90102",
---
+++ it/0/le/levacapsule.js
@@ -0,0 +1,3 @@
+Gr = [
+"Apriscatole con Levacapsule incorporato|Un Apriscatole con Levacapsule, per una vita facile in cucina!|2017/01/apriscatole-con-levacapsule-incorporato|data.oliviero.it/imgprodotto/apriscatole-con-levacapsule-420258_155155",
+];
