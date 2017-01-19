39b112c2c08cefbfe9eedbc43d30e6e236599bb9
microstorage <microstorage@users.noreply.github.com>
2017-01-19 15:58:00 +0100

    My commit

--- it/0/ca/casco-ciclismo.js
+++ it/0/ca/casco-ciclismo.js
@@ -1,3 +1,4 @@
+Tt = "Caschi da Ciclismo";
 Gr = [
 "Casco ciclismo Rush: leggero, sicuro, professionale|Dedicato a chi vuole raggiungere le migliori prestazioni|2017/01/casco-ciclismo-rush-leggero-sicuro-professionale|data.oliviero.it/imgprodotto/casco-ciclismo-rush_146291",
 ];
