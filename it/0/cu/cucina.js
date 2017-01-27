bbb4d0cdc945fb00ef6841a6be04970e36beaef3
microstorage <microstorage@users.noreply.github.com>
2017-01-27 20:11:35 +0100

    My commit

--- it/0/cu/cucina.js
+++ it/0/cu/cucina.js
@@ -1,4 +1,5 @@
 Gr = [
+"Tostapane: i 5 migliori a gennaio 2017|La top 5 dei migliori Tostapane venduti a gennaio 2017|2017/01/tostapane-i-5-migliori-a-gennaio-2017|",
 "Macchina da Caffè Lavazza Magia Bianco|Macchina da Caffè Magia Bianco: il miglior modo di preparare il caffè|2017/01/macchina-da-caffe-lavazza-magia-bianco|data.oliviero.it/imgprodotto/macchina-da-caffe-magia-bianco_155699",
 "Apriscatole con Levacapsule incorporato|Un Apriscatole con Levacapsule, per una vita facile in cucina!|2017/01/apriscatole-con-levacapsule-incorporato|data.oliviero.it/imgprodotto/apriscatole-con-levacapsule-420258_155155",
 ];
---
+++ it/0/to/tostiera.js
@@ -0,0 +1,3 @@
+Gr = [
+"Tostapane: i 5 migliori a gennaio 2017|La top 5 dei migliori Tostapane venduti a gennaio 2017|2017/01/tostapane-i-5-migliori-a-gennaio-2017|",
+];
