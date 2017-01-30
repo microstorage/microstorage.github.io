a56b8a0dc9fe114735e02d745460daa1a63e15f0
microstorage <microstorage@users.noreply.github.com>
2017-01-30 21:49:49 +0100

    My commit

--- it/0/ca/casa.js
+++ it/0/ca/casa.js
@@ -1,4 +1,5 @@
 Gr = [
+"2 Pentole a Pressione di nuova generazione: gennaio 2017|Le 2 Pentole a Pressione migliori in assoluto, valutate a gennaio 2017|2017/01/2-pentole-a-pressione-di-nuova-generazione-gennaio-2017|data.oliviero.it/imgprodotto/pentola-6l-clipsÃ²_139748",
 "I 10 Spremiagrumi migliori a gennaio 2017|La top 10 dei migliori Spremiagrumi subito disponibili: gennaio 2017|2017/01/i-10-spremiagrumi-migliori-a-gennaio-2017|data.oliviero.it/imgprodotto/spremiagrumi-39-cm-nero_156267",
 "Tostapane: i 5 migliori a gennaio 2017|La top 5 dei migliori Tostapane venduti a gennaio 2017|2017/01/tostapane-i-5-migliori-a-gennaio-2017|data.oliviero.it/imgprodotto/tostapane-tp10_154100",
 "Il Materassino con la Pompa Incorporata|Materassino Floccato con Pompa Incorporata: facile e comodo da gonfiare|2017/01/il-materassino-con-la-pompa-incorporata|data.oliviero.it/imgprodotto/materassino-floccato-con-pompa-incorporata_86164",
---
+++ it/0/pe/pentole.js
@@ -0,0 +1,3 @@
+Gr = [
+"2 Pentole a Pressione di nuova generazione: gennaio 2017|Le 2 Pentole a Pressione migliori in assoluto, valutate a gennaio 2017|2017/01/2-pentole-a-pressione-di-nuova-generazione-gennaio-2017|data.oliviero.it/imgprodotto/pentola-6l-clipsÃ²_139748",
+];
