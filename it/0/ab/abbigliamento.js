39f232e3bd8d4a80ff46fc55720fe5999638aa93
microstorage <microstorage@users.noreply.github.com>
2017-02-01 18:03:29 +0100

    My commit

--- it/0/ab/abbigliamento.js
+++ it/0/ab/abbigliamento.js
@@ -1,5 +1,5 @@
 Gr = [
-"Le 10 T-Shirt da Uomo più comprate: febbraio 2017|Le 10 T-Shirt da Uomo più utilizzate: la classifica aggiornata|2017/02/le-10-t-shirt-da-uomo-piu-comprate-febbraio-2017|data.oliviero.it/imgprodotto/t-shirt-donna-taglie-forti-uomo-510-800_57550",
+"Le 10 T-Shirt da Uomo più comprate: febbraio 2017|Le 10 T-Shirt da Uomo più utilizzate: la classifica aggiornata|2017/02/le-10-t-shirt-da-uomo-piu-comprate-febbraio-2017|data.oliviero.it/imgprodotto/t-shirt-uomo-bluette-essentials-linear-tee-s98734_159301",
 "Le 10 T-Shirt da Donna più comprate: febbraio 2017|La classifica aggiornata dele 10 T-Shirt da Donna più comprate|2017/02/le-10-t-shirt-da-donna-piu-comprate-febbraio-2017|data.oliviero.it/imgprodotto/t-shirt-donna-x20129_50282_big",
 "Tute Sportive da Uomo: le 10 migliori a febbraio 2017|La top 10 delle migliori Tute Sportive da Uomo sul mercato|2017/02/tute-sportive-da-uomo-le-10-migliori-a-febbraio-2017|data.oliviero.it/imgprodotto/tuta-sportiva-uomo-nero-e-bianco-832844-101_158090",
 "2 Pantaloni Sci Uomo da avere: gennaio 2017|I 2 Pantaloni Sci da Uomo che amerai dopo una giornata sulle piste|2017/01/2-pantaloni-sci-uomo-da-avere-gennaio-2017|data.oliviero.it/imgprodotto/pantaloni-sci-uomo-nero-0166g-4ko-99_155710",
--- it/0/t-/t-shirt.js
+++ it/0/t-/t-shirt.js
@@ -1,5 +1,5 @@
 Tt = "T-Shirt";
 Gr = [
-"Le 10 T-Shirt da Uomo più comprate: febbraio 2017|Le 10 T-Shirt da Uomo più utilizzate: la classifica aggiornata|2017/02/le-10-t-shirt-da-uomo-piu-comprate-febbraio-2017|data.oliviero.it/imgprodotto/t-shirt-donna-taglie-forti-uomo-510-800_57550",
+"Le 10 T-Shirt da Uomo più comprate: febbraio 2017|Le 10 T-Shirt da Uomo più utilizzate: la classifica aggiornata|2017/02/le-10-t-shirt-da-uomo-piu-comprate-febbraio-2017|data.oliviero.it/imgprodotto/t-shirt-uomo-bluette-essentials-linear-tee-s98734_159301",
 "Le 10 T-Shirt da Donna più comprate: febbraio 2017|La classifica aggiornata dele 10 T-Shirt da Donna più comprate|2017/02/le-10-t-shirt-da-donna-piu-comprate-febbraio-2017|data.oliviero.it/imgprodotto/t-shirt-donna-x20129_50282_big",
 ];
