c735889b69a2a291bdbbd9060e67000153e24cf2
microstorage <microstorage@users.noreply.github.com>
2017-02-03 17:27:53 +0100

    My commit

--- it/0/ri/ristorante.js
+++ it/0/ri/ristorante.js
@@ -1,3 +1,4 @@
 Gr = [
+"Le 10 Casseruole più comprate: febbraio 2017|La top 10 delle migliori Casseruole subito disponibili: febbraio 2017|2017/02/le-10-casseruole-piu-comprate-febbraio-2017|data.oliviero.it/imgprodotto/casseruola-20-cm-silver-2-maniglie_155959",
 "La top 5 dei migliori Tritatutto: gennaio 2017|La top 5 dei Tritatutto migliori, subito disponibili: gennaio 2017|2017/01/la-top-5-dei-migliori-tritatutto-gennaio-2017|data.oliviero.it/imgprodotto/tritatutto-tr20_155555",
 ];
