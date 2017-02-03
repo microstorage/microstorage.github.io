23cd398750f304a6a1d184ba43808bcda3bdcf30
microstorage <microstorage@users.noreply.github.com>
2017-02-03 19:36:32 +0100

    My commit

--- it/0/ci/cibo.js
+++ it/0/ci/cibo.js
@@ -1,4 +1,5 @@
 Gr = [
+"Forni a Microonde: i 3 migliori a febbraio 2017|La top 3 dei migliori Forni a Microonde, subito disponibili!|2017/02/forni-a-microonde-i-3-migliori-a-febbraio-2017|data.oliviero.it/imgprodotto/microonde-sinbo-smo-3637_155917",
 "Le 2 Macchine per Waffle che consigliamo: febbraio 2017|Le due migliori Macchine per Waffle, valutate a febbraio 2017|2017/02/le-2-macchine-per-waffle-che-consigliamo-febbraio-2017|data.oliviero.it/imgprodotto/macchina-waffle_155639",
 "Le 10 Casseruole più comprate: febbraio 2017|La top 10 delle migliori Casseruole subito disponibili: febbraio 2017|2017/02/le-10-casseruole-piu-comprate-febbraio-2017|data.oliviero.it/imgprodotto/casseruola-20-cm-silver-2-maniglie_155959",
 "Gelatiere: le 3 migliori a gennaio 2017|La top 3 delle migliori Gelatiere: gennaio 2017|2017/01/gelatiere-le-3-migliori-a-gennaio-2017|data.oliviero.it/imgprodotto/gelatiera_155838",
---
+++ it/0/gr/grill.js
@@ -0,0 +1,3 @@
+Gr = [
+"Forni a Microonde: i 3 migliori a febbraio 2017|La top 3 dei migliori Forni a Microonde, subito disponibili!|2017/02/forni-a-microonde-i-3-migliori-a-febbraio-2017|data.oliviero.it/imgprodotto/microonde-sinbo-smo-3637_155917",
+];
