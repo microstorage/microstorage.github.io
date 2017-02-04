c275677fb9d682f73ad1fa71a0a61258e869f3ce
microstorage <microstorage@users.noreply.github.com>
2017-02-04 16:40:30 +0100

    My commit

--- it/0/ba/baby-monitor.js
+++ it/0/ba/baby-monitor.js
@@ -1,4 +1,5 @@
 Gr = [
+"Le 10 Tettarelle consigliate: febbraio 2017|Le 10 migliori Tettarelle vendute a febbraio 2017|2017/02/le-10-tettarelle-consigliate-febbraio-2017|data.oliviero.it/imgprodotto/2-tettarelle-6-natural-flusso-pappa_66826",
 "Biberon: i 10 migliori a gennaio 2017|I 10 Biberon che vanno per la maggiore ora in commercio: gennaio 2017|2017/01/biberon-i-10-migliori-a-gennaio-2017|data.oliviero.it/imgprodotto/biberon-260-ml-rosa_53145",
 "Il primo Baby-Monitor a emissioni zero|Babycamera zero emissioni, per la sicurezza del tuo bambino|2017/01/il-primo-baby-monitor-a-emissioni-zero|data.oliviero.it/imgprodotto/babycamera-0-emissioni_132117",
 ];
--- it/0/co/colica.js
+++ it/0/co/colica.js
@@ -1,3 +1,4 @@
 Gr = [
+"Le 10 Tettarelle consigliate: febbraio 2017|Le 10 migliori Tettarelle vendute a febbraio 2017|2017/02/le-10-tettarelle-consigliate-febbraio-2017|data.oliviero.it/imgprodotto/2-tettarelle-6-natural-flusso-pappa_66826",
 "Biberon: i 10 migliori a gennaio 2017|I 10 Biberon che vanno per la maggiore ora in commercio: gennaio 2017|2017/01/biberon-i-10-migliori-a-gennaio-2017|data.oliviero.it/imgprodotto/biberon-260-ml-rosa_53145",
 ];
