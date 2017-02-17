157fd3595b86bdef9865d9d7a2dc96e3746759fe
microstorage <microstorage@users.noreply.github.com>
2017-02-17 15:01:16 +0100

    My commit

--- it/0/sm/smartphone.js
+++ it/0/sm/smartphone.js
@@ -1,4 +1,5 @@
 Gr = [
+"5 Telefonini Giocattolo per i bambini: febbraio 2017|I 5 Telefonini Giocattolo più divertenti, subito disponibili|2017/02/5-telefonini-giocattolo-per-i-bambini-febbraio-2017|data.oliviero.it/imgprodotto/telefonino-vibra-e-scatta_19804_big",
 "La fitness band automatica, la più pratica|Vivosmart HR: rileva automaticamente il tipo di attività e movimento|2017/01/la-fitness-band-automatica-la-piu-pratica|data.oliviero.it/imgprodotto/vivosmart-hr-010-01955-00_157684",
 "Radio ad energia solare con carica cellulare|Una radio solare davvero indispensabile per i nostri viaggi, e non solo|2017/01/radio-ad-energia-solare-con-carica-cellulare|data.oliviero.it/imgprodotto/radio-solare_83933",
 ];
---
+++ it/0/te/telefono-cellulare.js
@@ -0,0 +1,3 @@
+Gr = [
+"5 Telefonini Giocattolo per i bambini: febbraio 2017|I 5 Telefonini Giocattolo più divertenti, subito disponibili|2017/02/5-telefonini-giocattolo-per-i-bambini-febbraio-2017|data.oliviero.it/imgprodotto/telefonino-vibra-e-scatta_19804_big",
+];
