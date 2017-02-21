e9011f9ad749a86a2606c0c57ed0816e7ea6cdd1
microstorage <microstorage@users.noreply.github.com>
2017-02-21 15:54:10 +0100

    My commit

--- it/0/gi/giocattoli.js
+++ it/0/gi/giocattoli.js
@@ -1,4 +1,5 @@
 Gr = [
+"I 7 Tricicli Giocattolo più belli: febbraio 2017|I 7 Tricicli Giocattolo consigliati ora sul mercato: febbraio 2017|2017/02/i-7-tricicli-giocattolo-piu-belli-febbraio-2017|data.oliviero.it/imgprodotto/triciclo-skeggia-giallo_151818",
 "5 Treni Giocattolo divertentissimi: febbraio 2017|I 5 Treni Giocattolo più desiderati in commercio: febbraio 2017|2017/02/5-treni-giocattolo-divertentissimi-febbraio-2017|data.oliviero.it/imgprodotto/trenino-fun-park_135985",
 "4 Trattori Giocattolo da regalare: febbraio 2017|I 4 Trattori Giocattolo più belli, disponibili ora per l'acquisto|2017/02/4-trattori-giocattolo-da-regalare-febbraio-2017|data.oliviero.it/imgprodotto/trattore-con-pala_93991",
 "5 Telefonini Giocattolo per i bambini: febbraio 2017|I 5 Telefonini Giocattolo più divertenti, subito disponibili|2017/02/5-telefonini-giocattolo-per-i-bambini-febbraio-2017|data.oliviero.it/imgprodotto/telefonino-vibra-e-scatta_19804_big",
---
+++ it/0/tr/triciclo.js
@@ -0,0 +1,3 @@
+Gr = [
+"I 7 Tricicli Giocattolo più belli: febbraio 2017|I 7 Tricicli Giocattolo consigliati ora sul mercato: febbraio 2017|2017/02/i-7-tricicli-giocattolo-piu-belli-febbraio-2017|data.oliviero.it/imgprodotto/triciclo-skeggia-giallo_151818",
+];
