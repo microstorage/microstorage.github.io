339af8784465cd36ecf6244217585167b2737d5a
microstorage <microstorage@users.noreply.github.com>
2017-03-16 15:41:50 +0100

    My commit

---
+++ it/0/je/jeggings.js
@@ -0,0 +1,3 @@
+Gr = [
+"I 3 Jeggings selezionati: marzo 2017|La top 3 dei migliori Jeggings sul mercato a marzo 2017|2017/03/i-3-jeggings-selezionati-marzo-2017|data.oliviero.it/imgprodotto/jeggings-t2s4pc-00094_146864",
+];
--- it/0/pa/pantaloni-donna.js
+++ it/0/pa/pantaloni-donna.js
@@ -1,4 +1,5 @@
 Gr = [
+"I 3 Jeggings selezionati: marzo 2017|La top 3 dei migliori Jeggings sul mercato a marzo 2017|2017/03/i-3-jeggings-selezionati-marzo-2017|data.oliviero.it/imgprodotto/jeggings-t2s4pc-00094_146864",
 "I 6 Jeans da Ragazza più belli: marzo 2017|I 6 Jeans da Ragazza più carini disponibili ora per l'acquisto|2017/03/i-6-jeans-da-ragazza-piu-belli-marzo-2017|data.oliviero.it/imgprodotto/jeans-jr-tf15756_154640",
 "I 10 Jeans da Donna più amati: marzo 2017|La top 10 dei migliori Jeans da Donna sul mercato a marzo 2017|2017/03/i-10-jeans-da-donna-piu-amati-marzo-2017|data.oliviero.it/imgprodotto/jeans-donna-t2s4pc-00458_146855",
 "I 4 Pantaloni Sci da Donna consigliati: gennaio 2017|I 4 Pantaloni Sci da Donna più consigliati oggi sul mercato|2017/01/i-4-pantaloni-sci-da-donna-consigliati-gennaio-2017|data.oliviero.it/imgprodotto/pantaloni-sci-donna-bianco-0269g-4ko-01_155736",
