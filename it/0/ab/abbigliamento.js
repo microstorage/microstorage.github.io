fa4ab6b5e2d037e8279897189cce4bfab5dd523d
microstorage <microstorage@users.noreply.github.com>
2017-03-25 19:10:33 +0100

    My commit

--- it/0/ab/abbigliamento.js
+++ it/0/ab/abbigliamento.js
@@ -1,4 +1,5 @@
 Gr = [
+"I 10 Pantaloncini da Boxe migliori: marzo 2017|I 10 Pantaloncini da Boxe al top: la classifica aggiornata|2017/03/i-10-pantaloncini-da-boxe-migliori-marzo-2017|data.oliviero.it/imgprodotto/pantaloncino-da-boxe-ab734_93252",
 "4 Pantaloncini da Tennis scelti: marzo 2017|Marzo 2017: i 4 Pantaloncini da Tennis che più ci piacciono|2017/03/4-pantaloncini-da-tennis-scelti-marzo-2017|data.oliviero.it/imgprodotto/pantaloncini-tennis-uomo-blu-830817-410_162290",
 "Marzo 2017: i 10 Pantaloncini Sportivi più acquistati|I 10 Pantaloncini Sportivi più consigliati da chi li ha comprati: la guida|2017/03/marzo-2017-i-10-pantaloncini-sportivi-piu-acquistati|data.oliviero.it/imgprodotto/pantaloncini-sportivi-uomo-grigio-842267-091_161674",
 "I 6 Pantaloncini Running uomo più acquistati: marzo 2017|I 6 Pantaloncini Running uomo più consigliati oggi sul mercato|2017/03/i-6-pantaloncini-running-uomo-piu-acquistati-marzo-2017|data.oliviero.it/imgprodotto/pantaloncini-running-uomo-dry-challenger-644236-010_161398",
---
+++ it/0/bo/boxe.js
@@ -0,0 +1,3 @@
+Gr = [
+"I 10 Pantaloncini da Boxe migliori: marzo 2017|I 10 Pantaloncini da Boxe al top: la classifica aggiornata|2017/03/i-10-pantaloncini-da-boxe-migliori-marzo-2017|data.oliviero.it/imgprodotto/pantaloncino-da-boxe-ab734_93252",
+];
