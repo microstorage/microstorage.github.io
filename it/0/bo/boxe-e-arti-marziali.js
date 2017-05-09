d2c0eeed340c71511d4125f30c0eb02402a98bec
microstorage <microstorage@users.noreply.github.com>
2017-05-09 15:46:33 +0200

    My commit

--- it/0/bo/boxe-e-arti-marziali.js
+++ it/0/bo/boxe-e-arti-marziali.js
@@ -1,4 +1,5 @@
 Gr = [
+"Sacco Boxe e Arti Marziali Training 30 kg Nero|Pelle sintetica, riempitura a stracci triturati, certificati|2017/05/sacco-boxe-e-arti-marziali-training-30-kg-nero|data.oliviero.it/imgprodotto/sacco-training-30-kg-nero_159670",
 "Guantoni Boxe Revenge 8 Oz - GN069|Guantoni Boxe per chi desidera distinguersi con una grafica d'impatto|2017/04/guantoni-boxe-revenge-8-oz-gn069|data.oliviero.it/imgprodotto/guantoni-boxe-revenge-8-oz-gn069_154530",
 "Pantaloncino da Boxe AB772|Pantaloncino da MMA in microfibra top quality e poliestere...|2017/04/pantaloncino-da-boxe-ab772|data.oliviero.it/imgprodotto/pantaloncino-da-boxe-ab772_93249",
 ];
---
+++ it/0/sp/sphinx.js
@@ -0,0 +1,3 @@
+Gr = [
+"Sacco Boxe e Arti Marziali Training 30 kg Nero|Pelle sintetica, riempitura a stracci triturati, certificati|2017/05/sacco-boxe-e-arti-marziali-training-30-kg-nero|data.oliviero.it/imgprodotto/sacco-training-30-kg-nero_159670",
+];
