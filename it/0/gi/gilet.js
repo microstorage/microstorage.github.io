351fed1739c35955b4c79a8d34ba0cd2e7d7204b
microstorage <microstorage@users.noreply.github.com>
2017-10-12 17:21:15 +0200

    My commit

--- it/0/gi/gilet.js
+++ it/0/gi/gilet.js
@@ -1,4 +1,5 @@
 Gr = [
+"Gilet uomo a 4 bottoni, 2 tasche|Gilet uomo chiusura 4 bottoni, due tasche, cinturino posteriore|2017/10/gilet-uomo-a-4-bottoni-2-tasche|https://data.oliviero.it/imgprodotto/gilet-uomo_83013",
 "Gilet donna due tasche a filetto|Gilet donna, chiusura con un bottone, collo a stola in contrasto|2017/05/gilet-donna-due-tasche-a-filetto|data.oliviero.it/imgprodotto/gilet-donna_82991",
 "Le 10 Felpe Uomo di moda ora: marzo 2017|Le 10 Felpe Uomo più belle: la classifica aggiornata a marzo 2017|2017/03/le-10-felpe-uomo-di-moda-ora-marzo-2017|data.oliviero.it/imgprodotto/felpa-uomo-arancio-essentials-linear-s98773_159283",
 "La top 10 delle Felpe Donna: marzo 2017|Le 10 Felpe Donna più desiderate: la classifica di marzo 2017|2017/03/la-top-10-delle-felpe-donna-marzo-2017|data.oliviero.it/imgprodotto/felpa-donna-marrone-nero-s7wlrs1_160431",
